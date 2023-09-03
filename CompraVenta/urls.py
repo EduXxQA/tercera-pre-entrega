bc3b12291310f47a2e945330e1526db794ecd026
Eduardo Abreu <e.abreucamp@gmail.com>
2023-09-03 14:35:09 -0300

    fifth comit

--- CompraVenta/urls.py
+++ CompraVenta/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from CompraVenta.views import auto, stock_autos
+from CompraVenta.views import auto, stock_autos, camioneta
 
 
 urlpatterns = [
     path('actualiza-auto/<marca>/<modelo>/<anio>/', auto),
+    path('actualiza-camioneta/<marca>/<modelo>/<anio>/', camioneta),
     path('stock-autos/', stock_autos),
     
 ]
--- CompraVenta/views.py
+++ CompraVenta/views.py
@@ -12,20 +12,29 @@ def auto(req, marca, modelo, anio):
         <p><strong>Auto:</strong> {auto.marca} - <strong>Modelo:</strong> {auto.modelo} - <strong>Año:</strong> {auto.anio} - <strong>Actualizado!!!</strong></p>                  
     """)
     
+def camioneta(req, marca, modelo, anio):
+    auto = Camioneta(marca = marca, modelo = modelo, anio = anio)
+    auto.save()
+    
+    return HttpResponse(f"""
+        <p><strong>Camioneta:</strong> {auto.marca} - <strong>Modelo:</strong> {auto.modelo} - <strong>Año:</strong> {auto.anio} - <strong>Actualizado!!!</strong></p>                  
+    """)    
+    
+    
 def stock_autos(req):
     stock = Auto.objects.all()
     
     return render(req, "stock_autos.html", {"stock_autos": stock})
 
-def inicio(req):
+#def inicio(req):
     return HttpResponse("Vista de Inicio")
 
-def auto(req):
+#def auto(req):
     return HttpResponse("Vista de Autos")
 
-def camioneta(req):
+#def camioneta(req):
     return HttpResponse("Vista de Camionetas")
 
-def moto(req):
+#def moto(req):
     return HttpResponse("Vista de Motos")
 
